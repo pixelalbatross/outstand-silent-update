bb27c52bd90c3c5f327b4a2362e442f2f41722ba
Sérgio Santos <me@s3rgiosan.com>
2026-03-28 09:51:43 +0000

    Update org

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,12 @@
 
 All notable changes to this project will be documented in this file, per [the Keep a Changelog standard](http://keepachangelog.com/).
 
+## [1.1.0] - 2026-03-28
+
+### Changed
+
+- Update GitHub repository URL.
+
 ## [1.0.0] - 2026-03-27
 
 - Initial release.
--- plugin.php
+++ plugin.php
@@ -6,13 +6,13 @@
  * Plugin URI:        https://outstand.site/?utm_source=wp-plugins&utm_medium=outstand-silent-update&utm_campaign=plugin-uri
  * Requires at least: 6.7
  * Requires PHP:      8.2
- * Version:           1.0.0
+ * Version:           1.1.0
  * Author:            Outstand
  * Author URI:        https://outstand.site/?utm_source=wp-plugins&utm_medium=outstand-silent-update&utm_campaign=author-uri
  * License:           GPL-3.0-or-later
  * License URI:       https://spdx.org/licenses/GPL-3.0-or-later.html
  * Update URI:        https://outstand.site/
- * GitHub Plugin URI: https://github.com/outstand-labs/outstand-silent-update
+ * GitHub Plugin URI: https://github.com/pixelalbatross/outstand-silent-update
  * Text Domain:       outstand-silent-update
  * Domain Path:       /languages
  */
@@ -26,7 +26,7 @@ if ( ! defined( 'WPINC' ) ) {
 	die;
 }
 
-define( 'OUTSTAND_SILENT_UPDATE_VERSION', '1.0.0' );
+define( 'OUTSTAND_SILENT_UPDATE_VERSION', '1.1.0' );
 define( 'OUTSTAND_SILENT_UPDATE_BASENAME', plugin_basename( __FILE__ ) );
 define( 'OUTSTAND_SILENT_UPDATE_URL', plugin_dir_url( __FILE__ ) );
 define( 'OUTSTAND_SILENT_UPDATE_PATH', plugin_dir_path( __FILE__ ) );
@@ -38,7 +38,7 @@ if ( file_exists( OUTSTAND_SILENT_UPDATE_PATH . 'vendor/autoload.php' ) ) {
 }
 
 PucFactory::buildUpdateChecker(
-	'https://github.com/outstand-labs/outstand-silent-update/',
+	'https://github.com/pixelalbatross/outstand-silent-update/',
 	__FILE__,
 	'outstand-silent-update'
 )->setBranch( 'main' );
